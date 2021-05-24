19fd50f16b0d7a2bfc2cd48e92fdbeea458ed7eb
Vani Paridhyani <paridhya@adobe.com>
2021-05-24 15:02:14 +0530

    Starting LeetCodePractice :fingerscrossed

---
+++ Sandbox/LeetCodePractice/findMaxConsecutiveOnes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        hi = []
+        count = 0
+        for i in nums:
+            if i == 1:
+                count = count + 1
+                hi.append(count)
+            else:
+                count = 0
+                hi.append(count)
+        return max(hi)
+
+obj = Solution()
+print(obj.findMaxConsecutiveOnes([1,1,0,1,1,1]))
