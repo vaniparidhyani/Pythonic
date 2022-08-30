af311bd54b8e495f905c3f2fd4b0c7e484042708
Vani Paridhyani <paridhya@Vanis-NewMac.local>
2022-08-30 13:03:33 +0530

    Leetcode easy

--- Sandbox/LeetCodePractice/1480_running_sum_of_1d_array.py
+++ Sandbox/LeetCodePractice/1480_running_sum_of_1d_array.py
@@ -1,8 +1,9 @@
 class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
+    def runningSum(self, nums):
         out = []
         out.append(nums[0])
         for index in range(0,len(nums)-1):
             out.append(out[index]+nums[index+1])    
         return(out)
-print(Solution.runningSum())
+nums = [1,2,3,4]
+print(Solution().runningSum(nums))
